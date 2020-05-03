baabcd2193f46944041763417d147134a7fb7196
Pedro-Dalvit <dalvit.pedro@gmail.com>
2020-05-03 15:33:20 -0300

    se agrega utils y comunicacion con el broker

---
+++ GameBoy/src/GameBoy.h
@@ -0,0 +1,27 @@
+/*
+ * GameBoy.h
+ *
+ *  Created on: 3 may. 2020
+ *      Author: utnso
+ */
+
+#ifndef GAMEBOY_H_
+#define GAMEBOY_H_
+
+#include "utils/log.h"
+#include "utils/config.h"
+#include "utils/messageQueue.h"
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<commons/log.h>
+#include<commons/string.h>
+#include<commons/config.h>
+#include<readline/readline.h>
+
+
+void terminar_programa(int conexion, t_log* logger, t_config* config);
+
+
+
+#endif /* GAMEBOY_H_ */
---
+++ GameBoy/src/utils/config.h
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<signal.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<netdb.h>
+#include<string.h>
+#include<commons/config.h>
+
+t_config* leer_config(void);
