7e1fbda359bb862d8d49425487ab2d03ad6ddaef
Pedro-Dalvit <dalvit.pedro@gmail.com>
2020-05-03 14:40:16 -0300

    incluyo las commons

--- GameBoy/src/GameBoy.c
+++ GameBoy/src/GameBoy.c
@@ -10,8 +10,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <commons/temporal.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	char * tiempo =temporal_get_string_time();
+	puts(tiempo); /* prints !!!Hello World!!! */
 	return EXIT_SUCCESS;
 }
