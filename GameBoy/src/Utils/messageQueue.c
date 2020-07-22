b262612531f0443941030ff45ef365cececad767
lucilacanalini <lucilacanalini@gmail.com>
2020-07-21 23:04:47 -0300

    Detalles

--- GameBoy/src/Utils/messageQueue.c
+++ GameBoy/src/Utils/messageQueue.c
@@ -773,12 +773,6 @@ void recibir_mensaje(int socket_cliente){
 
 		break;
 	}
-/*
-	uint32_t identificador = 1;
-	int tamanio_paquete = 0;
-	void* a_enviar = enviar_ack(socket_cliente,codigo_de_operacion,identificador,&tamanio_paquete);
-	send(socket_cliente,a_enviar,tamanio_paquete,0); //no llega el ack
-*/
 }
 
 void enviar_ACK(int socket_broker, char* mensaje){
