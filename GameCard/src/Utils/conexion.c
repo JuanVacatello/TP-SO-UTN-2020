e985844b29ef797f18b1bd721d8975a0e3fd3276
lucilacanalini <lucilacanalini@gmail.com>
2020-07-23 01:57:33 -0300

    Envio de mensajes a Broker

--- GameCard/src/Utils/conexion.c
+++ GameCard/src/Utils/conexion.c
@@ -1,5 +1,7 @@
 #include "conexion.h"
 
+// Crear conexion (para Broker)
+
 int crear_conexion(char* ip, char* puerto)
 {
 	struct addrinfo hints;
@@ -27,7 +29,7 @@ int crear_conexion(char* ip, char* puerto)
 	return socket_cliente;
 }
 
-// RECIBIR MENSAJES DE GAMEBOY
+// Esperar y recibir mensajes de Gameboy
 
 void iniciar_espera_mensajes_Gameboy(void){
 
@@ -61,10 +63,10 @@ void iniciar_espera_mensajes_Gameboy(void){
 
     while(1)
 
-    	esperar_cliente(socket_servidor);
+    	esperar_gameboy(socket_servidor);
 }
 
-void esperar_cliente(int socket_servidor)
+void esperar_gameboy(int socket_servidor)
 {
 	struct sockaddr_in dir_cliente;
 
@@ -88,19 +90,20 @@ void serve_client(int* socket)
 	process_request(cod_op, *socket);
 }
 
-// ATENDER AL CLIENTE
-
 void process_request(op_code cod_op, int socket_cliente) {
 
 	switch(cod_op){
 		case 1:
-			recibir_new_pokemon(socket_cliente);
+			pthread_create(&hilo_new_pokemon, NULL, recibir_new_pokemon, socket_cliente);
+			pthread_detach(hilo_new_pokemon);
 			break;
 		case 3:
-			recibir_catch_pokemon(socket_cliente);
+			pthread_create(&hilo_catch_pokemon, NULL, recibir_catch_pokemon, socket_cliente);
+			pthread_detach(hilo_catch_pokemon);
 			break;
 		case 5:
-			recibir_get_pokemon(socket_cliente);
+			pthread_create(&hilo_get_pokemon, NULL, recibir_get_pokemon, socket_cliente);
+			pthread_detach(hilo_get_pokemon);
 			break;
 	}
 	//sem_post(&MUTEX_MENSAJES_GB);
@@ -124,10 +127,10 @@ void* serializar_paquete(t_paquete* paquete, int *bytes)
 	return a_enviar;
 }
 
-// RECEPCION DE MENSAJES DE BROKER
+// Conexion con Broker y espera de mensajes
 
 void new_pokemon_broker(){
-	int socket_broker = suscribirse_globalmente(NEW_POKEMON);
+	int socket_broker = suscribirse_globalmente(1);
 
 	while(1){
 
@@ -140,7 +143,6 @@ void new_pokemon_broker(){
 			//completar_logger(mensaje, "TEAM", LOG_LEVEL_INFO);
 
 			recibir_new_pokemon(socket_broker);
-
 		}
 	}
 }
@@ -159,7 +161,6 @@ void catch_pokemon_broker(){
 			//completar_logger(mensaje, "TEAM", LOG_LEVEL_INFO);
 
 			recibir_catch_pokemon(socket_broker);
-
 		}
 	}
 }
@@ -178,12 +179,11 @@ void get_pokemon_broker(){
 			//completar_logger(mensaje, "TEAM", LOG_LEVEL_INFO);
 
 			recibir_get_pokemon(socket_broker);
-
 		}
 	}
 }
 
-// BROKER - SUSCRIPTOR
+// Suscripciones a las colas de Broker
 
 int suscribirse_globalmente(op_code cola_a_suscribirse){
 
@@ -248,32 +248,47 @@ void* suscribirse_a_cola(int socket_broker, uint32_t cola_a_suscribirse, int* ta
 	return a_enviar;
 }
 
+// Recibir mensajes de GameBoy y Broker
 
 void recibir_new_pokemon(int socket_cliente){
 	uint32_t tamanio_buffer;
 	recv(socket_cliente, &tamanio_buffer, sizeof(uint32_t), MSG_WAITALL);
 
+		printf("El tamaño del buffer es %d", tamanio_buffer);
+
 	uint32_t mensaje_id;  // Hola chicos este mismo id lo van a tener que poner en el id_mensaje_correlativo del appeared correspondiente a este new salu2
 	recv(socket_cliente, &mensaje_id, sizeof(uint32_t), MSG_WAITALL);
+
+		printf("El mensaje_id es %d", mensaje_id);
 
 	uint32_t caracteresPokemon;
 	recv(socket_cliente, &caracteresPokemon, sizeof(uint32_t), MSG_WAITALL);
 
+		printf("El largo del pokemon es %d", caracteresPokemon);
+
 	char* pokemon = (char*)malloc(caracteresPokemon);
 	recv(socket_cliente, pokemon, caracteresPokemon, MSG_WAITALL);
+
+		printf("El pokemon es %s", pokemon);
 
 	uint32_t posX;
 	recv(socket_cliente, &posX, sizeof(uint32_t), MSG_WAITALL);
 
+		printf("La posicion en x es %d", posX);
+
 	uint32_t posY;
 	recv(socket_cliente, &posY, sizeof(uint32_t), MSG_WAITALL);
+
+		printf("La posicion en Y %d", posY);
 
 	uint32_t cantidad;
 	recv(socket_cliente, &cantidad, sizeof(uint32_t), MSG_WAITALL);
 
-	agregar_pokemon(pokemon, posX, posY, -1); // Holi no quiero tocar nada pero pasan 4 parametros pero la funcion es de 3
+		printf("La cantidaad es %d", cantidad);
 
-	responder_ack();
+	//agregar_pokemon(pokemon, posX, posY, -1); // Holi no quiero tocar nada pero pasan 4 parametros pero la funcion es de 3
+
+	//responder_ack(mensaje_id);
 }
 
 void recibir_catch_pokemon(int socket_cliente){
@@ -296,7 +311,7 @@ void recibir_catch_pokemon(int socket_cliente){
 	uint32_t posY;
 	recv(socket_cliente, &posY, sizeof(uint32_t), MSG_WAITALL);
 
-	responder_ack();
+	responder_ack(mensaje_id);
 
 }
 
@@ -314,40 +329,209 @@ void recibir_get_pokemon(int socket_cliente){
 	char* pokemon = (char*)malloc(caracteresPokemon);
 	recv(socket_cliente, pokemon, caracteresPokemon, MSG_WAITALL);
 
-	responder_ack();
+	responder_ack(mensaje_id);
 }
 
-// MENSAJES A BROKER
+// Enviar mensaje a Broker
 
-//falta todavia
+void enviar_appeared_pokemon(){
+	char* puerto_broker = obtener_puerto_broker();
+	char* ip_broker = obtener_ip_broker();
 
-// ACK
+	int socket_broker = crear_conexion(ip_broker,puerto_broker);
+	if(socket_broker == -1){
+		// Comportamiento default en caso de que no se pueda conectar a Broker
+		// Segun lo poco que lei en el enunciado solo hay que avisarlo por logs y seguir la ejecucion pero no se quizas
+		// hay mas info que esa
+	}
 
-void* enviar_ACK(int socket_broker, int* tamanio){
+	int tamanio_paquete = 0;
+	char* pokemon; // = no se de donde lo sacan pero igualen al pokemon
+	uint32_t posX; // = idem
+	uint32_t posY; // = idem
+	uint32_t id_mensaje_correlativo; // = id mensaje que venia en el new_pokemon
+	void* a_enviar = iniciar_paquete_serializado_AppearedPokemon(&tamanio_paquete, pokemon, posX, posY, id_mensaje_correlativo);
 
+	if(send(socket_broker,a_enviar,tamanio_paquete,0) == -1){
+		printf("Error en enviar por el socket");
+		exit(3);
+	}
+}
+
+void* iniciar_paquete_serializado_AppearedPokemon(int* tamanio_paquete, char* pokemon, uint32_t posX, uint32_t posY, uint32_t id_mensaje_correlativo){
 	t_paquete* paquete = malloc(sizeof(t_paquete));
-	paquete->codigo_operacion = 7;
+	paquete->codigo_operacion = 2;
 	paquete->buffer = malloc(sizeof(t_buffer));
-	paquete->buffer->stream = "ACK";
-	paquete->buffer->size = strlen(paquete->buffer->stream) + 1;
 
-	*tamanio = (paquete->buffer->size)+sizeof(op_code)+sizeof(uint32_t);
-	void* a_enviar = serializar_paquete(paquete,tamanio);
+	uint32_t caracteres_pokemon = strlen(pokemon)+1;
+	paquete->buffer->size = sizeof(uint32_t)+sizeof(uint32_t)+sizeof(uint32_t)+sizeof(uint32_t)+caracteres_pokemon;
 
+	void* stream = malloc(paquete->buffer->size);
+	int offset = 0;
+
+	memcpy(stream + offset, &caracteres_pokemon, sizeof(uint32_t));
+	offset += sizeof(uint32_t);
+
+	memcpy(stream + offset, pokemon, caracteres_pokemon);
+	offset += caracteres_pokemon;
+
+	memcpy(stream + offset, &posX, sizeof(uint32_t));
+	offset += sizeof(uint32_t);
+
+	memcpy(stream + offset, &posY, sizeof(uint32_t));
+	offset += sizeof(uint32_t);
+
+	memcpy(stream + offset, &id_mensaje_correlativo, sizeof(uint32_t));
+
+	paquete->buffer->stream = stream;
+
+	*tamanio_paquete = (paquete->buffer->size)+sizeof(op_code)+sizeof(uint32_t);
+	void* a_enviar = serializar_paquete(paquete, tamanio_paquete);
 	return a_enviar;
 }
 
-void responder_ack(){
+void enviar_caught_pokemon(){
+	char* puerto_broker = obtener_puerto_broker();
+	char* ip_broker = obtener_ip_broker();
+
+	int socket_broker = crear_conexion(ip_broker,puerto_broker);
+	if(socket_broker == -1){
+		// Comportamiento default en caso de que no se pueda conectar a Broker
+		// Segun lo poco que lei en el enunciado solo hay que avisarlo por logs y seguir la ejecucion pero no se quizas
+		// hay mas info que esa
+	}
+
+	int tamanio_paquete = 0;
+	uint32_t id_mensaje_correlativo; // = id mensaje que venia en el new_pokemon
+	uint32_t se_pudo_atrapar; // No se de donde sacan esto pero recuerden que si se pudo, hay que mandar un 1 y sino un -1
+	void* a_enviar = iniciar_paquete_serializado_CaughtPokemon(&tamanio_paquete, id_mensaje_correlativo, se_pudo_atrapar);
+
+	if(send(socket_broker,a_enviar,tamanio_paquete,0) == -1){
+		printf("Error en enviar por el socket");
+		exit(3);
+	}
+}
+
+void* iniciar_paquete_serializado_CaughtPokemon(int* tamanio_paquete, uint32_t id_mensaje_correlativo, uint32_t se_pudo_atrapar){
+	t_paquete* paquete = malloc(sizeof(t_paquete));
+	paquete->codigo_operacion = 4;
+	paquete->buffer = malloc(sizeof(t_buffer));
+	paquete->buffer->size = sizeof(uint32_t) + sizeof(uint32_t);
+
+	void* stream = malloc(paquete->buffer->size);
+	int desplazamiento = 0;
+
+	memcpy(stream + desplazamiento, &id_mensaje_correlativo, sizeof(uint32_t));
+	desplazamiento += sizeof(uint32_t);
+
+	memcpy(stream + desplazamiento, &se_pudo_atrapar, sizeof(uint32_t));
+
+	paquete->buffer->stream = stream;
+
+	*tamanio_paquete = (paquete->buffer->size)+sizeof(op_code)+sizeof(uint32_t);
+	void* a_enviar = serializar_paquete(paquete, tamanio_paquete);
+	return a_enviar;
+}
+
+void enviar_localized_pokemon(){
+	char* puerto_broker = obtener_puerto_broker();
+	char* ip_broker = obtener_ip_broker();
+
+	int socket_broker = crear_conexion(ip_broker,puerto_broker);
+	if(socket_broker == -1){
+		// Comportamiento default en caso de que no se pueda conectar a Broker
+		// Segun lo poco que lei en el enunciado solo hay que avisarlo por logs y seguir la ejecucion pero no se quizas
+		// hay mas info que esa
+	}
+
+	int tamanio_paquete = 0;
+		uint32_t id_mensaje_correlativo; // = id mensaje que venia en el new_pokemon
+		char* pokemon; // = a lo que sea que tengan que mandar
+		uint32_t cantidad_de_posiciones; // idem
+		void* a_enviar = iniciar_paquete_serializado_LocalizedPokemon(&tamanio_paquete, id_mensaje_correlativo, pokemon, cantidad_de_posiciones);
+
+	if(send(socket_broker,a_enviar,tamanio_paquete,0) == -1){
+		printf("Error en enviar por el socket");
+		exit(3);
+	}
+}
+
+void* iniciar_paquete_serializado_LocalizedPokemon(int* tamanio_paquete, uint32_t id_mensaje_correlativo, char* pokemon, uint32_t cantidad_de_posiciones){
+
+	t_paquete* paquete = malloc(sizeof(t_paquete));
+	paquete->codigo_operacion = 1;
+	paquete->buffer = malloc(sizeof(t_buffer));
+	uint32_t caracteres_pokemon = strlen(pokemon)+1;
+							// ID CORRELATIVO + CARACTERES + POKEMON + CANTIDAD + (POSX+POSY)*CANTIDAD
+	paquete->buffer->size = sizeof(uint32_t) + sizeof(uint32_t) + caracteres_pokemon + sizeof(uint32_t) + (2*sizeof(uint32_t)*cantidad_de_posiciones);
+
+	void* stream = malloc(paquete->buffer->size);
+	int desplazamiento = 0;
+
+	memcpy(stream + desplazamiento, &id_mensaje_correlativo, sizeof(uint32_t));
+	desplazamiento += sizeof(uint32_t);
+
+	memcpy(stream + desplazamiento, &caracteres_pokemon, sizeof(uint32_t));
+	desplazamiento += sizeof(uint32_t);
+
+	memcpy(stream + desplazamiento, pokemon, caracteres_pokemon);
+	desplazamiento += caracteres_pokemon;
+
+	memcpy(stream + desplazamiento, &cantidad_de_posiciones, sizeof(uint32_t));
+	desplazamiento += sizeof(uint32_t);
+
+	// memcpy(stream + desplazamiento, posxposy, tam_posxposy);
+	// La logica de las posiciones en las q se encuentra la hacemos otro dia juntos porque depende de como lo lean y etc
+
+	paquete->buffer->stream = stream;
+
+	tamanio_paquete = (paquete->buffer->size)+sizeof(op_code)+sizeof(uint32_t);
+	void* a_enviar = serializar_paquete(paquete, tamanio_paquete);
+	return a_enviar;
+}
+
+// ACK
+
+void responder_ack(uint32_t mensaje_id){
 	char* puerto_broker = obtener_puerto_broker();
 	char* ip_broker = obtener_ip_broker();
 
 	int socket_ack = crear_conexion(ip_broker,puerto_broker);
 
 	int tamanio = 0;
-	void* a_enviar = enviar_ACK(socket_ack, &tamanio);
+	void* a_enviar = enviar_ACK(socket_ack, &tamanio, mensaje_id);
 	send(socket_ack,a_enviar,tamanio,0);
 
 	free(a_enviar);
+}
+
+void* enviar_ACK(int socket_broker, int* tamanio, uint32_t mensaje_id){
+
+	t_paquete* paquete = malloc(sizeof(t_paquete));
+	paquete->codigo_operacion = 7;
+	paquete->buffer = malloc(sizeof(t_buffer));
+
+	char* mensaje = "ACK";
+	uint32_t caracteres_mensaje = strlen(mensaje) + 1;
+	paquete->buffer->size = sizeof(uint32_t) + sizeof(uint32_t) + caracteres_mensaje;
+	void* stream = malloc(paquete->buffer->size);
+	int offset = 0;
+
+	memcpy(stream + offset, &caracteres_mensaje, sizeof(uint32_t));
+	offset += sizeof(uint32_t);
+
+	memcpy(stream + offset, mensaje, caracteres_mensaje);
+	offset += caracteres_mensaje;
+
+	memcpy(stream + offset, &mensaje_id, sizeof(uint32_t));
+	offset += sizeof(uint32_t);
+
+	paquete->buffer->stream = stream;
+
+	*tamanio = (paquete->buffer->size)+sizeof(op_code)+sizeof(uint32_t);
+	void* a_enviar = serializar_paquete(paquete,tamanio);
+
+	return a_enviar;
 }
 
 char* recibir_mensaje(int socket_broker){
--- GameCard/src/Utils/conexion.h
+++ GameCard/src/Utils/conexion.h
@@ -40,33 +40,46 @@ typedef struct{
 } t_paquete;
 
 pthread_t thread;
+pthread_t hilo_new_pokemon;
+pthread_t hilo_catch_pokemon;
+pthread_t hilo_get_pokemon;
 
+// Crear conexion (para Broker)
 int crear_conexion(char* ip, char* puerto);
 
+// Esperar y recibir mensajes de Gameboy
 void iniciar_espera_mensajes_Gameboy(void);
-void esperar_cliente(int socket_servidor);
-void serve_client(int* socket);
-void process_request(op_code cod_op, int socket_cliente);
-
-void* serializar_paquete(t_paquete* paquete, int *bytes);
-void enviar_mensaje_a_broker(int socket_cliente, op_code codigo_operacion, char* argv[]);
-int suscribirse_globalmente(op_code cola_a_suscribirse);
-void* suscribirse_a_cola(int socket_broker, uint32_t cola_a_suscribirse, int* tamanio_paquete);
-
-void iniciar_espera_mensajes(void);
-void esperar_cliente(int socket_servidor);
+void esperar_gameboy(int socket_servidor);
 void serve_client(int* socket_cliente);
 void process_request(op_code cod_op, int socket_cliente);
 
+// Conexion con Broker y espera de mensajes
 void new_pokemon_broker();
 void catch_pokemon_broker();
 void get_pokemon_broker();
+
+// Suscripciones a las colas de Broker
+int suscribirse_globalmente(op_code cola_a_suscribirse);
+void* suscribirse_a_cola(int socket_broker, uint32_t cola_a_suscribirse, int* tamanio_paquete);
+
+// Recibir mensajes de GameBoy y Broker
 void recibir_new_pokemon(int socket_cliente);
 void recibir_catch_pokemon(int socket_cliente);
 void recibir_get_pokemon(int socket_cliente);
+void enviar_mensaje_a_broker(int socket_cliente, op_code codigo_operacion, char* argv[]);
 
-void* enviar_ACK(int socket_broker, int* tamanio);
-void responder_ack();
+// Enviar mensaje a Broker
+void enviar_appeared_pokemon(void);
+void* iniciar_paquete_serializado_AppearedPokemon(int* tamanio_paquete, char* pokemon, uint32_t posX, uint32_t posY, uint32_t id_mensaje_correlativo);
+void enviar_caught_pokemon(void);
+void* iniciar_paquete_serializado_CaughtPokemon(int* tamanio_paquete, uint32_t id_mensaje_correlativo, uint32_t se_pudo_atrapar);
+void enviar_localized_pokemon();
+void* iniciar_paquete_serializado_LocalizedPokemon(int* tamanio_paquete, uint32_t id_mensaje_correlativo, char* pokemon, uint32_t cantidad_de_posiciones);
+
+// Auxiliares
+void* serializar_paquete(t_paquete* paquete, int *bytes);
+void* enviar_ACK(int socket_broker, int* tamanio, uint32_t mensaje_id);
+void responder_ack(uint32_t mensaje_id);
 char* recibir_mensaje(int socket_broker);
 
 #endif /* UTILS_CONEXION_H_ */
