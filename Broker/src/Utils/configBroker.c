2303eda3f37b6a2a9b2f9571604ea331a6163cea
JuanVacatello <juan.vacatello@gmail.com>
2020-07-30 19:27:48 -0300

    correcciones

--- Broker/src/Utils/configBroker.c
+++ Broker/src/Utils/configBroker.c
@@ -2,7 +2,7 @@
 
 void leer_config(void)
 {
-	configBroker = config_create("/home/utnso/Documentos/Delibird/Broker/tp-2020-1c-wRAPPERS/Broker/Broker.config");
+	configBroker = config_create("Broker.config");
 
 	if(configBroker == NULL){
 		printf("No se pudo leer el archivo de configuración del Broker.");
