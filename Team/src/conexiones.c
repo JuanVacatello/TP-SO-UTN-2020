ebd272cba27651449df8415863f03801f82dfa8c
JuanVacatello <juan.vacatello@gmail.com>
2020-07-30 19:27:08 -0300

    correcciones

--- Team/src/conexiones.c
+++ Team/src/conexiones.c
@@ -39,7 +39,10 @@ void iniciar_espera_mensajes_Gameboy(void)
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
-    getaddrinfo(IP, PUERTO, &hints, &servinfo);
+    char* ip = obtener_ip_team();
+    char* puerto = obtener_puerto_team();
+
+    getaddrinfo(ip, puerto, &hints, &servinfo);
 
     for (p=servinfo; p != NULL; p = p->ai_next){
         if ((socket_servidor = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
