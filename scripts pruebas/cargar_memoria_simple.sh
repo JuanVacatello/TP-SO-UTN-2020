03e1247cc5874883a7951455c02f9e08b68b38df
Juan Vacatello <juan.vacatello@gmail.com>
2020-08-09 20:31:08 -0300

    Limpio master

--- scripts pruebas/cargar_memoria_simple.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-./gameboy BROKER GET_POKEMON Pikachu
-./gameboy BROKER GET_POKEMON Charmander
--- scripts pruebas/catch_pokemon_escenario_1_s1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-./gameboy TEAM APPEARED_POKEMON Pikachu 1 1
-./gameboy TEAM APPEARED_POKEMON Squirtle 9 7
-./gameboy TEAM APPEARED_POKEMON Onix 2 2
